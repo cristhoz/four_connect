08bb77d3998fd8fb716eb965bea9c9f095fc3d2f
Cristian de la Hoz <cristhoz@gmail.com>
2022-11-20 08:33:55 -0500

    feat: add result view struct file

---
+++ src/main/java/dev/delahoz/four_connect/views/ResultView.java
@@ -0,0 +1,34 @@
+package dev.delahoz.four_connect.views;
+
+import dev.delahoz.four_connect.config.properties.Properties;
+import dev.delahoz.four_connect.route.Router;
+import dev.delahoz.four_connect.route.Routes;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class ResultView implements Views {
+    private final Scene scene;
+
+    public ResultView() {
+        Properties properties = new Properties();
+
+        Label label = new Label("ResultView");
+        Button buttonOne = new Button("GoTo Home");
+        Button buttonTwo = new Button("GoTo Game");
+
+        buttonOne.setOnAction((event) -> Router.setRoute(Routes.Home));
+        buttonTwo.setOnAction((event) -> Router.setRoute(Routes.Game));
+
+        VBox layout = new VBox();
+        layout.getChildren().addAll(label, buttonOne, buttonTwo);
+
+        scene = new Scene(layout, properties.getScreenConfig().getViewSize().getWidth(), properties.getScreenConfig().getViewSize().getHeight());
+
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+}
