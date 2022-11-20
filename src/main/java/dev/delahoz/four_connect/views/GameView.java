5afbc050fe25ad45122d3c17b6b64bca826ba616
Cristian de la Hoz <cristhoz@gmail.com>
2022-11-20 08:32:25 -0500

    feat: add game view struct file

---
+++ src/main/java/dev/delahoz/four_connect/views/GameView.java
@@ -0,0 +1,31 @@
+package dev.delahoz.four_connect.views;
+
+import dev.delahoz.four_connect.config.properties.Properties;
+import dev.delahoz.four_connect.route.Router;
+import dev.delahoz.four_connect.route.Routes;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class GameView implements Views {
+    private final Scene scene;
+
+    public GameView() {
+        Properties properties = new Properties();
+
+        Label label = new Label("GameView");
+        Button button = new Button("GoTo Result");
+
+        button.setOnAction((event) -> Router.setRoute(Routes.Result));
+
+        VBox layout = new VBox();
+        layout.getChildren().addAll(label, button);
+
+        scene = new Scene(layout, properties.getScreenConfig().getViewSize().getWidth(), properties.getScreenConfig().getViewSize().getHeight());
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+}
