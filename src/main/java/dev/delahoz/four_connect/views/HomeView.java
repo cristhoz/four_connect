ced77ee2eba91286432d9cf5ac9773305e8867d8
Cristian de la Hoz <cristhoz@gmail.com>
2022-11-21 07:58:47 -0500

    feat: implement load fxml files

--- src/main/java/dev/delahoz/four_connect/views/HomeView.java
+++ src/main/java/dev/delahoz/four_connect/views/HomeView.java
@@ -1,31 +1,14 @@
 package dev.delahoz.four_connect.views;
 
 import dev.delahoz.four_connect.config.properties.Properties;
-import dev.delahoz.four_connect.route.Router;
-import dev.delahoz.four_connect.route.Routes;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 
-public class HomeView implements Views {
-    private final Scene scene;
-
+public class HomeView extends Views {
     public HomeView() {
         Properties properties = new Properties();
 
-        Label label = new Label("ResultView");
-        Button button = new Button("Go To Game");
-
-        button.setOnAction((event) -> Router.setRoute(Routes.Game));
-
-        VBox layout = new VBox();
-        layout.getChildren().addAll(label, button);
-
-        scene = new Scene(layout, properties.getScreenConfig().getViewSize().getWidth(), properties.getScreenConfig().getViewSize().getHeight());
-    }
-
-    public Scene getScene() {
-        return scene;
+        loadView("home-view.fxml");
+        scene = new Scene(layout, properties.getScreenConfig().getViewSize().getWidth(), properties.getScreenConfig()
+                .getViewSize().getHeight());
     }
 }
--- src/main/java/dev/delahoz/four_connect/views/Views.java
+++ src/main/java/dev/delahoz/four_connect/views/Views.java
@@ -1,7 +1,26 @@
 package dev.delahoz.four_connect.views;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 
-public interface Views {
-    Scene getScene();
+import java.io.IOException;
+
+public abstract class Views {
+    Parent layout;
+
+    Scene scene;
+
+    void loadView(String view) {
+        try {
+            FXMLLoader loader = new FXMLLoader(this.getClass().getResource(view));
+            layout = loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException("Error to load fxml for home-view", e);
+        }
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
 }
